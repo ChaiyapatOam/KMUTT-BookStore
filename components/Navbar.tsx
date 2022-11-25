2b452e63fb3d3d5a9f277ddb3d3e53fd8335c14f
Arthit3108 <arthit.nop@gmaile.com>
2022-11-26 02:59:45 +0700

    Update Nav

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,12 +4,12 @@ import Link from "next/link";
 
 const Navbar = () => {
   return <div>
-  <div className="bg-header py-5">
-    <img className="h-[50px] px-6" src="/kmutt-logo.png"/>
+  <div className="bg-header p-6 ">
+    <img className="h-[60px] pl-4" src="/kmutt-logo.png"/>
   </div>
   <div className="bg-navbar text-white font-bold">
-    <div className="px-5 py-3">
-      <a className="px-1" href="">Home</a>
+    <div className="p-6 py-3">
+      <a className="px-4" href="">Home</a>
       <a className="px-3" href="">Check oder</a>
       <a className="px-3" href="">New Student</a>
     </div>

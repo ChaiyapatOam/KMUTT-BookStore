c1a0b60eddd71e8c579272e65e9d03db1d6cc9d0
Arthit3108 <arthit.nop@gmaile.com>
2022-11-26 16:06:29 +0700

    Footer

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,11 +7,18 @@ const Navbar = () => {
   <div className="bg-header p-6 ">
     <img className="h-[60px] pl-4" src="/kmutt-logo.png"/>
   </div>
-  <div className="bg-navbar text-white font-bold">
-    <div className="p-6 py-3">
+  <div className="bg-navbar text-white font-bold flex ">
+    <div className="p-6 py-3 flex-shirnk">
       <a className="px-4" href="">Home</a>
+    </div>
+    <div className="p-6 py-3 flex-shirnk">
       <a className="px-3" href="">Check oder</a>
+    </div>
+    <div className="p-6 py-3 flex-grow">
       <a className="px-3" href="">New Student</a>
+    </div>
+    <div className="p-6 py-3 ">
+      <img className="bg-red-500 px-3 w-[70px] h-[70px]" src="/search-icon.png" alt="" />
     </div>
   </div>
   </div>
